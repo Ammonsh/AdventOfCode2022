923dc2fd00f5852833ad9453f99ba4b953487d34
Ammon Shurtz <ammonsh@outlook.com>
2022-12-01 12:33:04 -0700

    Finished day-1

---
+++ day1/main/target/debug/deps/main-1f3d6ba3399ff6fb.d
@@ -0,0 +1,5 @@
+/Users/ammonshurtz/side-projects/AdventOfCode2022/day1/main/target/debug/deps/main-1f3d6ba3399ff6fb.rmeta: src/main.rs
+
+/Users/ammonshurtz/side-projects/AdventOfCode2022/day1/main/target/debug/deps/main-1f3d6ba3399ff6fb.d: src/main.rs
+
+src/main.rs:
---
+++ day1/main/target/debug/deps/main-6b0aa38428e8aec6.d
@@ -0,0 +1,5 @@
+/Users/ammonshurtz/side-projects/AdventOfCode2022/day1/main/target/debug/deps/main-6b0aa38428e8aec6: src/main.rs
+
+/Users/ammonshurtz/side-projects/AdventOfCode2022/day1/main/target/debug/deps/main-6b0aa38428e8aec6.d: src/main.rs
+
+src/main.rs:
